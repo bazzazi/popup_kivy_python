c48d96bbef39b6ff845bba696b8ee1776701c271
Mohammad Ali Bazzazi <81759694+bazzazi@users.noreply.github.com>
2021-11-06 07:46:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+# Snake Game in python using kivy for mobile & desktop
+# Developer: Mohammad Ali Bazzazi (me)
+
+print("""
+_______________     __________   _______   _______    _________   _______    #
+|              \   |          |        /         /   |         |        /   
+|               |  |          |       /         /    |         |       /     |
+|              /   |          |      /         /     |         |      /      |
+|_____________|    |__________|     /         /      |_________|     /       |
+|              \   |          |    /         /       |         |    /        |
+|               |  |          |   /         /        |         |   /         |
+|              /   |          |  /         /         |         |  /          |
+|_____________/    |          | /_______  /_______   |         | /______     |
+""")
+print("*********************************************************************************")
+print("*"+" "*79+"*")
+print("*  Copyright of Mohammad Ali Bazzazi, 2021 ©                                    *")
+print("*"+" "*79+"*")
+print("*  https://www.youtube.com/channel/UCeLKoNs3c72Vc-OG3uNQxGw?sub_confirmation=1  *")
+print("*"+" "*79+"*")
+print("*********************************************************************************")
+
+################## START ##################
 from kivy.app import App
 from kivy.uix.widget import Widget
 
@@ -7,3 +30,4 @@ class mainApp(App):
 	pass 
 
 mainApp().run()
+################## END ##################
